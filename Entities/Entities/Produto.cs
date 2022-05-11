c23569dd2f4c00e5c0758909498a70831fe286f8
Nicolly21 <nicollyoliveira21@hotmail.com>
2022-05-10 21:47:54 -0300

    Estrutura inicial

---
+++ Entities/Entities/Produto.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Entities
+{
+    [Table("Produtos")]
+    public class Produto : Notifies
+    {
+        [Column("COD_PRODUTO")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("NOME")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("DESCRICAO")]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Column("IMAGEM")]
+        [Display(Name = "Descrição")]
+        public byte[] Imagem { get; set; }
+
+        [Column("VALOR")]
+        [Display(Name = "Valor")]
+        public decimal Valor { get; set; }
+
+    }
+}
---
+++ PedidoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PedidoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
